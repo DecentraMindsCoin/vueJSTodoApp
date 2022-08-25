dc21b34f867d08e0bbe8d095a8704b3a133161e1
DecentraMindsCoin <decentraminds@gmail.com>
2022-08-24 23:18:50 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref, onMounted, computed, watch} from 'vue';
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null)
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return a.createdAt - b.createdAt
+}))
+
+</script>
+
+<template>
+<main class="app">
+  <section class="greeting">
+    <h2 class ="title" >
+Whats up!, <input type="text" placeholder="name here" v-model="name" /> 
+    </h2>
+  </section>
+
+</main>
+</template>
+
+<style scoped>
+
+</style>
